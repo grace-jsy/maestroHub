5f16b38e1d7eaa1af4604ee7ad722412bdcd60c8
grace-jsy <sseo.oee@gmail.com>
2026-02-21 22:14:19 +0900

    first commit

---
+++ src/main/java/com/grace/maestrohub/MaestroHubApplication.java
@@ -0,0 +1,13 @@
+package com.grace.maestrohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaestroHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaestroHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grace/maestrohub/MaestroHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grace.maestrohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaestroHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
